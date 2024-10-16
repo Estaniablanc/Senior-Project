f0c2d619fab7fbd5668d577b1cdac1273efe3140
Estaniablanc <71726093+Estaniablanc@users.noreply.github.com>
2024-10-16 10:49:01 -0400

    Initial commit

---
+++ MyOnlineCloset/login.js
@@ -0,0 +1,10 @@
+function showRegister() {
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("register-section").style.display = "block";
+  }
+  
+  function showLogin() {
+    document.getElementById("register-section").style.display = "none";
+    document.getElementById("login-section").style.display = "block";
+  }
+  
